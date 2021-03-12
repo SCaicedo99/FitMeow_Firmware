ecde5ad5831b34b584e989a3dff8a9c55177ef31
SCaicedo <sebascaicedo-01@hotmail.com>
2021-03-12 12:32:38 -0500

    Created separate files for UART functionality

---
+++ UART.c
@@ -0,0 +1,39 @@
+#include <avr/io.h>
+#include <util/delay.h>
+#include <stdint.h>       // needed for uint8_t
+#include "UART.h"
+
+void UART_Init(unsigned char ubrr){
+	// Set Baud rate
+	UCSR0A = (1<<U2X0); // Double the UART Trans speed
+	
+	UBRR0H = (unsigned char)(ubrr>>8);
+	UBRR0L = (unsigned char)ubrr;
+
+	UCSR0C = (3<<UCSZ00); // 8data, 1stop bit, Asynchronous mode, No parity
+	
+	// Enable receiver and transmitter
+	UCSR0B = (1<<RXEN0)|(1<<TXEN0)|(1<<RXCIE0);
+	UCSR0B &= ~(1<<UDRIE0); 
+}
+
+unsigned char UART_ReceiveChar(){
+	// Wait for data to be received
+	while( !(UCSR0A & (1<<RXC0)) )
+	;
+	return UDR0;
+}
+
+void UART_TransmitChar(unsigned char data){
+	// Wait for empty transmit buffer
+	while( !(UCSR0A & (1<<UDRE0)) )
+	;
+	UDR0 = data;
+}
+
+void UART_TransmitStr(char* StringPtr){	
+	while(*StringPtr != 0x00){
+		UART_TransmitChar(*StringPtr);    //Using the simple send function we send one char at a time
+		StringPtr++;
+	}        //We increment the pointer so we can read the next char
+}
